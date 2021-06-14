fc2216b0bfc4fef830275bdc3333bca2ef72eaf6
nicobovina <nicobovina@gmail.com>
2021-06-14 14:35:49 -0300

    agregada get activities

--- api/src/routes/activityRouter.js
+++ api/src/routes/activityRouter.js
@@ -10,12 +10,10 @@ const { Activity } = require('../db.js');
 // siéntanse libres de dividir entre archivos si lo necesitan
 activityRouter.post('/', async (req, res) => {
 	const { name, difficulty, season, duration, countries } = req.body;
-	// const activityCreated = await Activity.create({
-	// 	name,
-	// 	difficulty,
-	// 	season,
-	// 	duration,
-	// });
+
+	// Revisa si existe la actividad que se desea crear.
+	// En caso de no existir esa actividad, la crea.
+	// En caso de existir, la unica funcionalidad es permitir agregar nuevos paises
 	const [activityCreated, created] = await Activity.findOrCreate({ where: { name: name.toLowerCase() } } );
 	if (created){
 		activityCreated.difficulty = difficulty;
@@ -28,5 +26,28 @@ activityRouter.post('/', async (req, res) => {
 
 	return res.status(200).json(activityCreated);
 });
+
+
+
+// Muestra las actividades creadas.
+// En caso de recibir como parametro un name, busca en la BD de datos aquellas actividades con ese nombre
+// y devuelve lo encontrado
+activityRouter.get('/', async (req, res) => {
+	const { name } = req.query;
+	let activities;
+
+	try{
+		if (!name) activities = await Activity.findAll();
+		else {
+			activities = await Activity.findAll({ where: { name: { [Op.iLike]: `%${name}%` } } });
+			// En caso que no se encuentren actividades que incluyan el string name
+			if (activities.length === 0)	return res.status(404).send('Activities not founded');
+		}
+		return res.status(200).json(activities);
+	} catch (err) {
+		return res.send(err);
+	}
+});
+
 
 module.exports = activityRouter;
