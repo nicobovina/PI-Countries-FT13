20058bf2aa634104dedc99157140aad4b74bae3c
nicobovina <nicobovina@gmail.com>
2021-06-29 23:30:00 -0300

    refactoring

--- api/src/routes/Activities.js
+++ api/src/routes/Activities.js
@@ -11,38 +11,31 @@ const { Activity } = require('../db.js');
 activities.post('/', async (req, res) => {
 	const { name, difficulty, season, duration, countries } = req.body;
 	let activities;
-	// Revisa si existe la actividad que se desea crear.
-	// En caso de no existir esa actividad, la crea.
-	// En caso de existir, la unica funcionalidad es permitir agregar nuevos paises
-	const [activityCreated, created] = await Activity.findOrCreate({ 
-		where: { name: name.toLowerCase() },
-		defaults: {
-			difficulty,
-			season,
-			duration
-		}});
-	console.log(activityCreated);
-	// const countriesParsed = countries.substr(1,countries.length-2).split(',');
-	// activityCreated.addCountries(countriesParsed);
-	activityCreated.addCountries([...countries]);
-	activities = await Activity.findAll({attributes: ['name']});
-	return res.status(200).json(activities);
+
+	try{
+		const [activity, created] = await Activity.findOrCreate({ 
+			where: { name: name.toLowerCase() },
+			defaults: {
+				difficulty,
+				season,
+				duration
+			}});
+		activity.addCountries([...countries]);
+		activities = await Activity.findAll({attributes: ['name']});
+		return res.status(200).json(activities);
+	} catch(err){
+		return res.send(err);
+	}
 });
 
 
 
 // Muestra las actividades creadas.
-activities.get('/', async (req, res) => {
-	// const { name } = req.query;
+activities.get('/', async (req, res) => {	
 	let activities;
 	try{
-		activities = await Activity.findAll({attributes: ['name']});
-		// if (!name) activities = await Activity.findAll();
-		// else {
-		// 	activities = await Activity.findAll({ where: { name: { [Op.iLike]: `%${name}%` } } });
-		// 	// En caso que no se encuentren actividades que incluyan el string name
-		// 	if (activities.length === 0)	return res.status(404).send('Activities not founded');
-		// }
+		activities = await Activity.findAll();
+		if (activities.length === 0)	return res.status(404).send('Not Found');
 		return res.status(200).json(activities);
 	} catch (err) {
 		return res.send(err);
@@ -51,3 +44,5 @@ activities.get('/', async (req, res) => {
 
 
 module.exports = activities;
+
+
--- api/src/routes/Countries.js
+++ api/src/routes/Countries.js
@@ -7,16 +7,10 @@ var countries = Router();
 
 const { Country, Activity } = require('../db.js');
 
-// escriban sus rutas acá
-// siéntanse libres de dividir entre archivos si lo necesitan
-
-
 // Obtengo todos los paises, y mostrar los 10 primeros.
 // Por el body, el parametro 'index' indica a partir de que
 // paises se tiene que enviar.
 countries.get('/', async (req, res) => {
-	// Variable que controla hasta que pais se envio.
-	// const { index } = req.query;
 	const { name, continent } = req.query;
 	let filters = {};
 	let countries;
@@ -32,8 +26,9 @@ countries.get('/', async (req, res) => {
 			},
 			order: [['name', 'ASC']]
 		});
-		// En caso que no se encuentren paises que incluyan el string name
-		if (countries.length === 0)	return res.status(200).send(countries);
+		// En caso que no se encuentren paises con los parametros buscados
+		// if (countries.length === 0)	return res.status(200).send(countries);
+		if (countries.length === 0)	return res.status(404).send('Not Found');
 		return res.status(200).send(countries);
 	} catch (err) {
 		return res.send(err);
@@ -44,13 +39,15 @@ countries.get('/', async (req, res) => {
 // que se corresponde con el ID del pais en la BD
 countries.get('/:idPais', async (req, res) => {
 	const { idPais } = req.params;
-	const country = await Country.findByPk(idPais.toUpperCase(), {include: Activity});
-
-	// En caso que el id recibido no coincida con un pais que se encuentre en BD
-	if (!country)	return res.status(404).send('Country not founded');
-
-	return res.status(200).json(country);
-})
+	try{
+		const country = await Country.findByPk(idPais.toUpperCase(), {include: Activity});
+		// En caso que el id recibido no coincida con un pais que se encuentre en BD
+		if (!country)	return res.status(404).send('Not Found');
+		return res.status(200).json(country);
+	} catch (err) {
+		return res.send(err);
+	}
+});
 
 
 
